f659effcd9a14d1e5b68c87f894a5aa53ddfcd15
Michael <oloomike77@gmail.com>
2023-08-17 05:09:14 -0700

    eggs

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function that returns the weighted average of intergers"""
+
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    number = 0
+    avg = 0
+
+    for num in my_list:
+        number += num[0] * num[1]
+        avg += num[1]
+
+    return (number / avg)
