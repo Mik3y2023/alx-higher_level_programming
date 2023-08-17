94c8ce9f6fda8c21f6d242073cefab5a3ac31870
Michael <oloomike77@gmail.com>
2023-08-17 05:25:42 -0700

    hops

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,44 +1,25 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_number):
-  """Converts a Roman numeral to an integer.
+def roman_to_int(roman_string):
 
-  Args:
-    roman_number: A string representing a Roman numeral.
+    if roman_string is None or type(roman_string) is not str:
+        return 0
 
-  Returns:
-    The integer value of the Roman numeral.
-  """
-  roman_numeral_dict = {
-      "I": 1,
-      "V": 5,
-      "X": 10,
-      "L": 50,
-      "C": 100,
-      "D": 500,
-      "M": 1000,
-      "IV": 4,
-      "IX": 9,
-      "XL": 40,
-      "XC": 90,
-      "CD": 400,
-      "CM": 900,
-  }
+    num = 0
+    last = 0
+    roman_letters = [
+        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+        ['X', 10], ['V', 5], ['I', 1]
+    ]
 
-  total = 0
-  i = 0
-  while i < len(roman_number):
-    current_value = roman_numeral_dict[roman_number[i]]
-    next_value = None
-    if i + 1 < len(roman_number):
-      next_value = roman_numeral_dict[roman_number[i + 1]]
+    for letter in roman_string:
+        for let_r in roman_letters:
+            if letter == let_r[0]:
+                if last == 0 or last >= let_r[1]:
+                    num += let_r[1]
+                elif last < let_r[1]:
+                    num += let_r[1] - (last * 2)
 
-    if next_value and current_value < next_value:
-      total -= current_value
-    else:
-      total += current_value
-    i += 1
+                last = let_r[1]
 
-  return total
-
-
+    return num
