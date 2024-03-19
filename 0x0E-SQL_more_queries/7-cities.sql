7a16a8a1ee16ae30d1063d469d6116a601b7e173
Mik3y2023 <oloomike77@gmail.com>
2024-03-19 12:13:40 -0700

    ref

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,7 +8,7 @@
 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-	PRIMARY KEY `(id)`,
+	PRIMARY KEY(`id`),
        	`id` INT UNIQUE AUTO_INCREMENT NOT NULL, 
 	`state_id` INT NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
