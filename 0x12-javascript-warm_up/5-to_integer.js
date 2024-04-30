f4e6ef14be8689e602237ca195ec045f87f527c2
Mik3y2023 <oloomike77@gmail.com>
2024-04-30 02:50:08 -0700

    loop

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,16 @@
+
+#!/usr/bin/node
+// script that prints My number: <first argument converted to int>
+
+const argument = process.argv[2];
+
+if (!isNaN(argument)) {
+  const number = parseInt(argument);
+  if (!isNaN(number)) {
+    console.log(`My number: ${number}`);
+  } else {
+    console.log('Not a number');
+  }
+} else {
+  console.log('Not a number');
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const array = [['C is fun'], ['Python is cool'], ['Javascript is amazing']];
+array.forEach(item => {
+	console.log(item[0]);
+});
