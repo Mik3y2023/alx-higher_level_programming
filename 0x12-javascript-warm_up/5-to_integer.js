17197566442886a4f4e7ddce968c52ceae374905
Mik3y2023 <oloomike77@gmail.com>
2024-04-30 11:33:33 -0700

    super

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,16 +1,4 @@
-
 #!/usr/bin/node
-// script that prints My number: <first argument converted to int>
-
-const argument = process.argv[2];
-
-if (!isNaN(argument)) {
-  const number = parseInt(argument);
-  if (!isNaN(number)) {
-    console.log(`My number: ${number}`);
-  } else {
-    console.log('Not a number');
-  }
-} else {
-  console.log('Not a number');
-}
+const { argv } = require('process');
+const converted = Number(argv[2]);
+if (isNaN(converted)) { console.log('Not a number'); } else { console.log(`My number: ${converted}`); }
